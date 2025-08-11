11371009d30c82102008860d0e0fc4fd1d87b4cd
Utkarsh <3107utkarshpathak@gmail.com>
2025-08-11 14:57:01 +0530

    read and write

--- fs/fs.js
+++ fs/fs.js
@@ -12,5 +12,21 @@ fs.readFile('f2.txt', function (err, data) { // this callback is called error fi
     if(err){
         console.log("error:",err);
     }
-    console.log("this is file 2 data "+ data)
+    console.log("this is file 2 data "+ data) 
 })
+console.log("end");
+
+
+//update 
+//sync
+fs.writeFileSync('f4.txt','I am f4 data')
+//async
+fs.writeFile('f1.txt','Utkarsh Pathak','utf-8',(err)=>{
+    if(err) throw err;
+    console.log('File updated!');
+});
+// fs.appendFile, fs.appendFileSync
+// write methods
+//fs.writeFileSync,fs.writeFile
+//fs.unlinkSync
+//fs.unlink
